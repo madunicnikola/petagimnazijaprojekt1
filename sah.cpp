bb6b7ad0594bef22e0dfbb86f67652cfa7f512a7
Nikola Madunić <madunicnikola@protonmail.com>
2022-05-25 16:50:47 +0200

    Update sah.cpp

--- sah.cpp
+++ sah.cpp
@@ -426,18 +426,6 @@ private:
     char pokretIgraca;
 };
 
-template <typename T>
-constexpr T calculatePower(T value, unsigned power) {
-    return power == 0 ? 1 : value * calculatePower(value, power-1);
-}
-template <class T, T V, T N, class I = std::make_integer_sequence<T, N>>
-struct power;
-
-template <class T, T V, T N, T... Is>
-struct power<T, V, N, std::integer_sequence<T, Is...>> {
-   static constexpr T value = (static_cast<T>(1) * ... * (V * static_cast<bool>(Is + 1)));
-};
-
 int main(){
     sahovnicaGlavna igra;
     igra.pocetak();
